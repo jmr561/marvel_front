7948566889cfa913042506989459aae1e76fcf71
Josh <joshua.m.rivas@gmail.com>
2022-02-19 17:30:37 +0100

    tweaks on home page and faves system before submission

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,11 @@
+import marvelImg from "../assets/The_Marvel_Universe.webp";
+
 const Home = () => {
-  return <div className="home-page-main-container">Home page</div>;
+  return (
+    <div className="home-page-main-container">
+      <img src={marvelImg} alt="marvel-characters-image" />
+    </div>
+  );
 };
 
 export default Home;
